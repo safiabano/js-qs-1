c5de58809fb1fcd592660987d24c565e52fabf31
safiabano <safiabano736@gmail.com>
2023-04-04 10:26:53 +0500

    qs-1

---
+++ js class 01/app.js
@@ -0,0 +1,27 @@
+// let myName="safia"; // var // variable //let
+// alert(myName);
+//variables rules
+//1.legal or illigal name
+//legal
+//lastName//camelcase
+//allowed special characters
+//_ $
+//illigal
+//last name
+//#/
+//reserved words
+//data types
+//string
+//all types special character
+////a-z in ""
+//any letter word sentences are string in javascrript
+//numbers -7 45 1 4.77
+//boolean 
+//let isTodayMonday=true
+
+
+//================= assignmentwork==============
+
+let myData ="Eror! please enter a valid password.";
+alert(myData)
+
